833c22508c873b0954345796bf56e7543cee8e42
trainode30 <deepakprajapati@successive.tech>
2019-04-15 16:29:34 +0530

    fix check statement

--- expressEngine/db/seedData.js
+++ expressEngine/db/seedData.js
@@ -4,23 +4,24 @@ import { User } from '../model';
 export default async function seed() {
   const res = await DBService.count(User);
 
-  if (res <= 0) {
-    try {
-      const admin = {
-        email: 'admin@gmail.com',
-        password: 'Admin@123',
-        role: 'system-admin',
-        companyName: 'nodeXperts',
-        contactName: '',
-        contactTitle: '',
-        city: 'Noida',
-        country: 'India',
-        phone: '',
-        fax: '',
-      };
-      await DBService.create(User, admin);
-    } catch (err) {
-      throw new Error(err);
-    }
+  if (res > 0) {
+    return;
+  }
+  try {
+    const admin = {
+      email: 'admin@gmail.com',
+      password: 'Admin@123',
+      role: 'system-admin',
+      companyName: 'nodeXperts',
+      contactName: '',
+      contactTitle: '',
+      city: 'Noida',
+      country: 'India',
+      phone: '',
+      fax: '',
+    };
+    await DBService.create(User, admin);
+  } catch (err) {
+    throw new Error(err);
   }
 }
