b1b1055fb47a91d952f9e75ffb66bc6107567765
chiragSuccessive <46670321+chiragSuccessive@users.noreply.github.com>
2019-04-16 18:33:37 +0530

    Update errorHandler.js

--- expressEngine/lib/errorHandler/errorHandler.js
+++ expressEngine/lib/errorHandler/errorHandler.js
@@ -4,6 +4,6 @@ export default function errorHandler(err, req, res) {
   const { message, status } = err;
   res.json({
     error: message || error.undefinedError,
-    status,
+    status || 500,
   });
 }
