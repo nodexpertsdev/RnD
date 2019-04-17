799805148bceb0d02dd396ce40d1a570828daf04
chiragSuccessive <chirag.arora@successive.tech>
2019-04-17 12:20:17 +0530

    fix: remove consoles

--- expressEngine/lib/errorHandler/errorHandler.js
+++ expressEngine/lib/errorHandler/errorHandler.js
@@ -1,11 +1,7 @@
 import error from '../../cms/common';
 
 export default function errorHandler(err, req, res, next) {
-  console.log(':::::::::::::ERROR::::::::::::::::::', typeof err, err);
-
   const { message, status } = err;
-  console.log(':::::::::::::::::::::::error::::::::::::::', JSON.stringify(err, Object.getOwnPropertyNames(err)));
-  console.log('::::::::::::::MESSAGE:::::::::::::::', JSON.stringify(message));
   res.json({
     error: message || error.undefinedError,
     status: status || 500,
