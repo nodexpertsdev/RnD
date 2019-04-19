2da6892358ee43d3c72d838ad8c2408a12841fab
ayushj117 <ayush.jha@successive.tech>
2019-04-19 15:35:32 +0530

    feat: refactor schema of customer(#91)

--- expressEngine/customer/service/customer.js
+++ expressEngine/customer/service/customer.js
@@ -9,27 +9,29 @@ import { error, success } from '../../cms/customer';
 
 class Service extends BaseService {
     register = async (data) => {
-        try {
-            const { firstName, lastName, city, country, contactNo } = data;
-            //const requiredFields = ["firstName", "lastName", "city", "country", "contactNo"];
+      try {
+        const {
+          name, email, city, country, contactNo,
+        } = data;
+        // const requiredFields = ["firstName", "lastName", "city", "country", "contactNo"];
 
-            const isExist = await DBService.findOne(Customer, { firstName, lastName });
-            if (isExist) {
-                return error.alreadyRegistered;
-            }
-
-            const customer = await DBService.create(Customer, {
-                firstName,
-                lastName,
-                city,
-                country,
-                contactNo,
-            });
-
-            return this.success(customer, success.customerRegistered);
-        } catch (err) {
-            return this.error(err);
+        const isExist = await DBService.findOne(Customer, { email });
+        if (isExist) {
+          return error.alreadyRegistered;
         }
+
+        const customer = await DBService.create(Customer, {
+          name,
+          email,
+          city,
+          country,
+          contactNo,
+        });
+
+        return this.success(customer, success.customerRegistered);
+      } catch (err) {
+        return this.error(err);
+      }
     }
 }
 
