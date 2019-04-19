ad9214b1db5598f3aeb54af0c6f2622c33e113a5
mayank-nx <41339919+mayank-nx@users.noreply.github.com>
2019-04-19 11:21:07 +0530

    Add: live route, live request, Update: not found route

--- expressEngine/lib/routeHelper.js
+++ expressEngine/lib/routeHelper.js
@@ -1,16 +1,39 @@
 class RouteHelper {
-  healthCheck = (req, res) => {
-    res.status(200).json({
-      status: 'ok',
-      message: 'Health is good'
-    });
-  };
+  /**
+   * @desc To get the live route
+   * @param prefix string
+   *
+   * @return string
+   */
+  static liveRoute(prefix = "") {
+    const route = "live";
+    if (prefix) {
+      return `/${prefix}/${route}`;
+    }
+    return `/${route}`;
+  }
+
+  /**
+   * @desc To handle the live request
+   * @param req request object
+   * @param res response object
+   *
+   * @return res
+   */
+  static liveRequest(req, res) {
+    res.send(true);
+  }
   
-  notFoundRoute = (req, res) => {
+  /**
+   * @desc To handle the live request
+   * @param req request object
+   * @param res response object
+   *
+   * @return res
+   */
+  static notFound(req, res) {
     res.status(404).json({
-      error: 'Not Found',
-      status: 404,
-      message: 'Route Not Found'
+      error: "Route Not Found"
     });
   };
 };
