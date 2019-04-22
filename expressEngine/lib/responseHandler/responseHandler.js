21bd7c6b8ea1758f9c0f81a4e42ac34c7002ddcf
chiragSuccessive <46670321+chiragSuccessive@users.noreply.github.com>
2019-04-22 12:37:33 +0530

    Update responseHandler.js

--- expressEngine/lib/responseHandler/responseHandler.js
+++ expressEngine/lib/responseHandler/responseHandler.js
@@ -1,7 +1,7 @@
 import { error as commonError, success } from '../../cms/common';
 
 class responseHandler {
-  success = ({ data = null, message = success.defaultSuccessMsg }) => ({ data, message });
+  success = ({ data = true, message = success.defaultSuccessMsg }) => ({ data, message });
 
   error = ({ error = commonError.undefinedError, status = 500 }) => ({ error, status });
 
