5967fedf10a7167d30c8dd5851cd88f821d527d7
amanjotSuccessive <43950341+amanjotSuccessive@users.noreply.github.com>
2019-04-23 19:13:52 +0530

    Update orderItems.js

--- expressEngine/orderItems/controller/orderItems.js
+++ expressEngine/orderItems/controller/orderItems.js
@@ -7,8 +7,6 @@ import { OrderItemsService } from '../service/index';
 class OrderItemsController extends BaseController {
   create = async (data) => {
     try {
-      console.log('Inside Register OrderItems Method');
-
       return await OrderItemsService.registerorderItems(data);
     } catch (err) {
       return err;
