5a11d63e967c52bbb1c7b1f2c5739c4c16cd4f39
chiragSuccessive <46670321+chiragSuccessive@users.noreply.github.com>
2019-04-24 16:34:37 +0530

    Update success.js

--- expressEngine/cms/order/success.js
+++ expressEngine/cms/order/success.js
@@ -1,5 +1,4 @@
 export default {
   orderGenerated: 'Your order is generated successfully',
   orderUpdated: 'Order updated successfully',
-  orderCanceled: 'Your order is canceled successfully',
 };
