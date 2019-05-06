a17babf29ebbc483b9e0503898f29d45358a28c9
chiragSuccessive <chirag.arora@successive.tech>
2019-05-06 14:56:54 +0530

    test: WIP

---
+++ expressEngine/test/data/index.js
@@ -0,0 +1,5 @@
+
+import error from './error';
+import success from './success';
+
+export { error, success };
---
+++ expressEngine/test/helper.spec.js
@@ -0,0 +1,16 @@
+import { connect } from 'mongoose';
+import inMemoryDB from './inMemoryDb';
+
+let db;
+
+before(async () => {
+  const { mongoUri } = await inMemoryDB();
+  db = await connect(
+    mongoUri,
+    { useCreateIndex: true, useNewUrlParser: true },
+  );
+});
+
+after(async () => {
+  db.disconnect();
+});
