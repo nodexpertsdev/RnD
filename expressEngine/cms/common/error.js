b0189c576ba38a70853ff80b6d24b9792bec62de
chiragSuccessive <46670321+chiragSuccessive@users.noreply.github.com>
2019-05-13 17:16:49 +0530

    Update error.js

--- expressEngine/cms/common/error.js
+++ expressEngine/cms/common/error.js
@@ -1,7 +1,7 @@
 export default {
   alreadyUpdated: 'Data provided is already updated',
   emptyData: 'Please provide the data to update',
-  noRecord: 'no record found',
+  noRecord: 'No record found',
   unableToUpdate: 'Unable to update',
   undefinedError: 'Got some error',
 };
